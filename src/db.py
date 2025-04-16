6eef7865316108b9ed7669f56ffcd9d5fbb06f91
GAKiknadze <gakiknadze@yandex.ru>
2025-04-16 14:31:17 +0300

    Added db connection

---
+++ src/db.py
@@ -0,0 +1,34 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+AsyncSessionLocal: sessionmaker[AsyncSession]  # type:ignore[type-var]
+
+
+def init_db(uri: str) -> None:
+    global AsyncSessionLocal
+
+    async_engine = create_async_engine(uri, echo=True, future=True)
+
+    AsyncSessionLocal = sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+        autocommit=False,
+        autoflush=False,
+    )  # type:ignore[call-overload]
+
+
+@asynccontextmanager
+async def get_db() -> AsyncGenerator[AsyncSession]:
+    global AsyncSessionLocal  # noqa: F824
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception as exc:
+            await session.rollback()
+            raise exc
+        finally:
+            await session.close()
